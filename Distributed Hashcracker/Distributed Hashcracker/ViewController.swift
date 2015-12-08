7c98583978ea50695cd032efe1fbae810d1d34d1
pascal <pascal@monkey-mailer.de>
2015-12-08 13:26:15 +0100

    basic project setup

---
+++ Distributed Hashcracker/Distributed Hashcracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Distributed Hashcracker
+//
+//  Created by Pascal Schönthier on 08.12.15.
+//  Copyright © 2015 Pascal Schönthier. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
