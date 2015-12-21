2da83491c9a9bea4b23b7cf384d0d6d5ff5f0320
pascal <pascal@monkey-mailer.de>
2015-12-21 18:46:50 +0100

    added some initial UI

--- Distributed Hashcracker/Distributed Hashcracker/ViewController.swift
+++ Distributed Hashcracker/Distributed Hashcracker/ViewController.swift
@@ -11,6 +11,23 @@ import Cocoa
 class ViewController: NSViewController {
 
     @IBOutlet weak var isManager: NSButton!
+    @IBOutlet var serverAdressField: NSTextField!
+    @IBOutlet var passwordField: NSTextField!
+    @IBOutlet var hashAlgorithmSelected: NSPopUpButton!
+    
+    
+    @IBAction func isServerButtonPressed(sender: NSButton) {
+        if sender.state == 1 {
+            serverAdressField.stringValue = "127.0.0.1"
+            serverAdressField.enabled = false
+        } else {
+            serverAdressField.stringValue = ""
+            serverAdressField.enabled = true
+        }
+        
+        // TODO: Setup Server
+    }
+    
     
     @IBAction func StartButtonPressed(sender: NSButton) {}
     
