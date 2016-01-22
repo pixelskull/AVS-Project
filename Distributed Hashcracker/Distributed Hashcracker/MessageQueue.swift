541efc9b6a3406119d911f25fe26bebf83e233b8
pixelskull <pascal.schoenthier@th-koeln.de>
2016-01-22 13:46:12 +0100

    added semaphore to MessageQueue and added Documentation Notes

--- Distributed Hashcracker/Distributed Hashcracker/MessageQueue.swift
+++ Distributed Hashcracker/Distributed Hashcracker/MessageQueue.swift
@@ -8,41 +8,57 @@
 
 import Cocoa
 
-
-
-protocol Initializable: class { init() }
-
 class MessageQueue {
 
     let notificationCenter = NSNotificationCenter.defaultCenter()
     var messages:[Message] = [Message]()
     
-    
     static let sharedInstance = MessageQueue()
+    
+    let semaphore = dispatch_semaphore_create(1)
     
     init() {
         notificationCenter.addObserver(self, selector: "put:", name: "putMessage", object: nil)
     }
 
+    /**
+     appends new message to MessageQueue (Blocking)
+     
+     - parameter message: Message to append
+    */
     func put(message:Message) {
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
         messages.append(message)
+        dispatch_semaphore_signal(semaphore)
     }
     
+    /**
+     get first Message if MessageQueue is not empty (Blocking)
+     
+     - returns: first message in list when not empty otherwise nil
+    */
     func get() -> Message? {
         guard let firstElement = messages.first else { return nil }
-        
+        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
         messages = messages.dropFirst().map { $0 }
+        dispatch_semaphore_signal(semaphore)
         return firstElement
     }
     
+    /**
+     get first Message if MessageQueue is not empty (Blocking)
+     
+     - returns: first message in list when not empty otherwise nil
+    */
     func poll() -> Message? {
-        guard messages.count > 0 else { return nil }
-        
-        let firstElement = messages.first!
+        guard let firstElement = messages.first else { return nil }
         messages = messages.dropFirst().map { $0 }
         return firstElement
     }
     
+    /**
+     add notify worker when new Message is appended (not implemented)
+    */
     func notify(notificationName:String) {}
     
     deinit {
