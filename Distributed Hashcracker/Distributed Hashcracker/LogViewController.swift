11d31716a538af2e807d25a1a3ed4866cdc3d165
pixelskull <pascal.schoenthier@th-koeln.de>
2016-02-08 16:08:45 +0100

    fixed small bug in LogViewController now with NSTextView the last line is focused

--- Distributed Hashcracker/Distributed Hashcracker/LogViewController.swift
+++ Distributed Hashcracker/Distributed Hashcracker/LogViewController.swift
@@ -10,22 +10,29 @@ import Cocoa
 
 class LogViewController: NSViewController {
     
-    @IBOutlet var logTextField: NSTextField!
+//    @IBOutlet var logTextField: NSTextField!
+    @IBOutlet var logTextView: NSTextView!
     
     let notificationCenter = NSNotificationCenter.defaultCenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        logTextField.stringValue += "Application started \n"
+        logTextView.editable = false 
+        logTextView.string = "Application started \n"
         notificationCenter.addObserver(self, selector: "updateLogTextField:", name: "updateLog", object: nil)
     }
     
     func updateLogTextField(notification:AnyObject?) {
         guard let message = notification!.object else {
-            logTextField.stringValue += "unrecognized message send \n"
+            logTextView.string! += "unrecognized message send \n"
             return
         }
-        logTextField.stringValue += "\(String(message!))\n"
+        logTextView.string! += "\(String(message!))\n"
+        
+        // scroll to end
+        let textViewStringLength = logTextView.string!.characters.count
+        let range:NSRange = NSMakeRange(textViewStringLength, 0)
+        logTextView.scrollRangeToVisible(range)
     }
     
     deinit {
