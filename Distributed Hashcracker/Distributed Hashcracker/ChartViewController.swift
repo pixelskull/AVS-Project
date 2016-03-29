5b371411b264c916216f689c0d476de8c93dcfb6
pixelskull <pascal.schoenthier@th-koeln.de>
2016-03-29 14:19:40 +0200

    basic linechart implementation

--- Distributed Hashcracker/Distributed Hashcracker/ChartViewController.swift
+++ Distributed Hashcracker/Distributed Hashcracker/ChartViewController.swift
@@ -14,14 +14,44 @@ struct HashValue {
     var hashes:Int
 }
 
-class ChartViewController: NSViewController {
+class ChartViewController: NSViewController, ChartViewDelegate {
     
+    let chart = LineChartView()
+    let data = [1.0, 7.0, 3.0, 5.0, 3.0, 4.0, 2.0]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        chart.frame = view.frame
+        chart.drawGridBackgroundEnabled = false
+        view.addSubview(chart)
         
+        setChartData(data)
 //        NSTimer.scheduledTimerWithTimeInterval(0.2, target: self, selector: "update", userInfo: nil, repeats: true)
 //        let notificationName = Constants.NCValues.addHashValue
 //        notificationCenter.addObserver(self, selector: "addHashChartValues:", name: notificationName, object: nil)
+    }
+    
+    func setChartData(data:[Double]) {
+        
+        var ydata = [ChartDataEntry]()
+        for i in 0..<data.count {
+            ydata.append(ChartDataEntry(value: data[i], xIndex: i))
+        }
+        
+        let dataSet = LineChartDataSet(yVals: ydata, label: "computed hashes")
+        dataSet.cubicIntensity = 0.2
+        dataSet.drawCubicEnabled = true
+        dataSet.drawCirclesEnabled = false
+        dataSet.lineWidth = 1.0
+        dataSet.setColor(NSColor.redColor())
+        dataSet.fillColor = NSColor.redColor()
+        dataSet.drawFilledEnabled = true
+        dataSet.fillAlpha = 0.6
+        dataSet.drawHorizontalHighlightIndicatorEnabled = false
+        
+        let chartData = LineChartData(xVals: ["1", "2", "3", "4", "5", "6", "7"], dataSet: dataSet)
+        
+        chart.data = chartData
     }
 }
